fa781013247bdb3353e62d8f894eefa8ccbd916c
Mihaela Ciobanu <mihaela.ciobanu03@gmail.com>
2019-06-11 15:38:22 +0100

    testing api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import React, { Component }  from 'react';
 import './App.css';
-import DataTabs from "./tabs.js";
+// import DataTabs from "./Tabs.js";
+// import myRequest from "./data/api";
 
+
+let url = 'https://content.guardianapis.com/search?order-by=newest&api-key=test'
 
 
   class App extends Component {
-    state = {
-      tabData: [
-        { label: "Taco", content: "Perhaps the greatest dish ever invented." },
-        {
-          label: "Burrito",
-          content:
-            "Perhaps the greatest dish ever invented but bigger and with rice."
-        }
-      ]
-    } 
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        // tabTitle: {sectionName},
+        // body: {webTitle}
+        tabData: [ ]
+      }
+    }
+
+    componentDidMount() {
+      // this.myRequest()
+
+      fetch(url)
+        // .then(response => {console.log(response)})
+        .then(response => response.json())
+        // .then(data => {
+        //   console.log('Request succeeded with JSON response', data.response.results)
+        // })
+        .then(data => this.setState({ tabData: data.response.results }))
+        .catch(error => {
+          console.log('Request failed', error)
+        })
+    }
 
     render() {
+      const {tabData} = this.state
+      console.log("tabData=>", tabData)
+      const tabList = tabData.length ? (
+        tabData.map(tab => {
+          return (
+            <div key={tab.id} >
+              <span>{tab.sectionName}</span>
+            </div>
+          )
+        })
+      ) 
+      : (<div>0</div>
+        )
       return (
         <div className="App">
-          <DataTabs data={this.state.tabData} />
+          {tabList}
+          {/* <DataTabs data={this.state.tabData} /> */}
         </div>
       );
     }
